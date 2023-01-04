48e970cd200b3873c9925562daf658e03bf5c573
Vladimir Turomsha <zver010810@gmail.com>
2023-01-04 17:20:18 +0300

    start models creating

---
+++ Entities/Models/Concert.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Concert
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public DateTime Date { get; set; }
+        public int TicketsNumber { get; set; }
+
+    }
+}
---
+++ Entities/Models/Ticket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public int? SeatNumber { get; set; }
+        public string District { get; set; } = null!;
+    }
+}

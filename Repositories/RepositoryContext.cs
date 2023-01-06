90a4b158fbfecb6759f0c531fa52318c7c9eed93
Vladimir Turomsha <zver010810@gmail.com>
2023-01-06 13:15:49 +0300

    add db sets

--- Repositories/RepositoryContext.cs
+++ Repositories/RepositoryContext.cs
@@ -13,6 +13,12 @@ namespace Repositories
         }
 
         public DbSet<ConcertType>? ConcertTypes { get; set; }
+        public DbSet<Artist>? Artists { get; set; }
+        public DbSet<Concert>? Concerts { get; set; }
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Ticket>? Tickets { get; set; }
+        public DbSet<Place>? Places { get; set; }
+        public DbSet<Booking>? Bookings { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
